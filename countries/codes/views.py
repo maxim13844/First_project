80804132df6a7917a8dc4837070e61dadbd00e5a
maxim13844 <maxim13844@gmail.com>
2023-08-03 16:30:05 +0300

    create index.html

--- countries/codes/views.py
+++ countries/codes/views.py
@@ -2,12 +2,15 @@ from django.http import HttpResponse
 
 from codes.models import Country
 
+from django.shortcuts import render
 
 def get_country_name(request):
     if 'code' not in request.GET:
         return HttpResponse('code param is required')
     code_input = request.GET['code'].upper()
-    return HttpResponse(Country.objects.filter(code=code_input).first() or 'Country not found')
+    country = Country.objects.filter(code=code_input).first()
+    return render(request, "codes/index.html", {'country_key': country})
+
 
 
 
